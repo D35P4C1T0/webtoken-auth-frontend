d71644e0be544746c688189bc0b5389479942453
Matteo <skewre>
2020-03-30 08:52:37 +0200

    Hardcoded backend url added

--- src/Auth.js
+++ src/Auth.js
@@ -1,9 +1,9 @@
-import axios from 'axios'
+import axios from "axios"
 
 class Auth {
   constructor() {
     this.authenticated = false
-    this.TOKEN = ''
+    this.TOKEN = ""
   }
 
   login(credentials, cb) {
@@ -14,30 +14,34 @@ class Auth {
 
     // let url = "http://localhost:3000/users/" + credentials.username
 
-    if (!process.env.backend_address) {
-      console.log('no backend address')
-    }
+    // if (!process.env.backend_address) {
+    //   console.log("no backend address")
+    // }
 
-    let url = process.env.backend_address + '/users/' + credentials.username
+    // let url = process.env.backend_address + "/users/" + credentials.username
+    let url =
+      "https://webtoken-mockup--d35p4c1t0.repl.co" +
+      "/users/" +
+      credentials.username
     // console.log(url)
 
     axios({
-      method: 'post',
+      method: "post",
       url: url,
       data: content,
       config: { headers: {} }
     })
       .then(response => {
-        console.log('RESPONSE RECEIVED', response)
+        console.log("RESPONSE RECEIVED", response)
         if (response.data.authenticated === true) {
-          console.log(content.username, 'is authenticated and has logged in')
+          console.log(content.username, "is authenticated and has logged in")
           this.authenticated = true
           this.TOKEN = response.data.token
           cb()
         }
       })
 
-      .catch(err => console.log('AXIOS ERROR', err))
+      .catch(err => console.log("AXIOS ERROR", err))
   }
 
   signup(credentials, cb) {
@@ -48,27 +52,28 @@ class Auth {
 
     // let url = 'http://localhost:3000/users'
 
-    if (!process.env.backend_address) {
-      console.log('no backend address')
-    }
+    // if (!process.env.backend_address) {
+    //   console.log("no backend address")
+    // }
 
-    let url = process.env.backend_address + '/users'
+    // let url = process.env.backend_address + "/users"
+    let url = "https://webtoken-mockup--d35p4c1t0.repl.co" + "/users"
     // console.log(url)
 
     axios({
-      method: 'post',
+      method: "post",
       url: url,
       data: content,
       config: { headers: {} }
     })
       .then(response => {
-        console.log('RESPONSE RECEIVED', response)
-        if (response.statusText === 'OK') {
+        console.log("RESPONSE RECEIVED", response)
+        if (response.statusText === "OK") {
           this.authenticated = true
           cb()
         }
       })
-      .catch(err => console.log('AXIOS ERROR', err))
+      .catch(err => console.log("AXIOS ERROR", err))
   }
 
   logout(cb) {
